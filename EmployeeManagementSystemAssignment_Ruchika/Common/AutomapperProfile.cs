9eb8ba6a730ed22b5f6f796d5afb8d11fb416318
RuchikaDhudum <149051307+RuchikaDhudum@users.noreply.github.com>
2024-06-23 20:53:49 +0530

    Add files via upload

---
+++ EmployeeManagementSystemAssignment_Ruchika/Common/AutomapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EmployeeManagementSystemAssignment_Ruchika.Entities;
+using EmployeeManagementSystemAssignment_Ruchika.Model;
+
+namespace EmployeeManagementSystemAssignment_Ruchika.Common
+{
+    public class AutomapperProfile:Profile
+        
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<EmployeeADEntity, EmployeeADModel>().ReverseMap();
+            CreateMap<WorkInfo_, WorkInfo_Model>().ReverseMap();
+            CreateMap<IdentityInfo_, IdentityInfo_Model>().ReverseMap();
+            CreateMap<PersonalDetails_, PersonalDetails_Model>().ReverseMap();
+        }
+    }
+}
---
+++ EmployeeManagementSystemAssignment_Ruchika/CosmosDB/CosmosDBService.cs
@@ -0,0 +1,60 @@
+﻿using EmployeeManagementSystemAssignment_Ruchika.Common;
+using EmployeeManagementSystemAssignment_Ruchika.Entities;
+using Microsoft.Azure.Cosmos;
+
+namespace EmployeeManagementSystemAssignment_Ruchika.CosmosDB
+{
+    public class CosmosDBService : ICosmosDBService
+    {
+        public CosmosClient _cosmosClient;
+        private readonly Container _container;
+
+        public CosmosDBService()
+        {
+            _cosmosClient = new CosmosClient(Credentials.CosmosEndPoint, Credentials.PrimaryKey);
+            _container = _cosmosClient.GetContainer(Credentials.DataBaseName, Credentials.ContainerName);
+        }
+
+        public async Task<EmployeeBDEntity> AddEmployeeBD(EmployeeBDEntity employee)
+        {
+            var response = await _container.CreateItemAsync(employee);
+            return response;
+        }
+
+        public async Task<List<EmployeeBDEntity>> GetAllEmployeeBD()
+        {
+            var response = _container.GetItemLinqQueryable<EmployeeBDEntity>(true).Where(q => q.Active == true && q.Archived == false && q.DocumentType == "employeeBD").ToList();
+            return response;
+        }
+
+        public async Task<EmployeeBDEntity> GetEmployeeBDByUId(string uId)
+        {
+            var response = _container.GetItemLinqQueryable<EmployeeBDEntity>(true).Where(q => q.UId == uId && q.Active == true && q.Archived == false && q.DocumentType == "employeeBD").FirstOrDefault();
+            return response;
+        }
+
+        public async Task ReplaceAsync(dynamic entity)
+        {
+            var response = await _container.ReplaceItemAsync(entity, entity.Id);
+        }
+
+        //Employee aditional deatils
+        public async Task<EmployeeADEntity> AddEmployeeAD(EmployeeADEntity employee)
+        {
+            var response = await _container.CreateItemAsync(employee);
+            return response;
+        }
+
+        public async Task<List<EmployeeADEntity>> GetAllEmployeeAD()
+        {
+            var response = _container.GetItemLinqQueryable<EmployeeADEntity>(true).Where(q => q.Active == true && q.Archived == false && q.DocumentType == "employeeAD").ToList();
+            return response;
+        }
+
+        public async Task<EmployeeADEntity> GetEmployeeADByUId(string EmployeeBDUId)
+        {
+            var response = _container.GetItemLinqQueryable<EmployeeADEntity>(true).Where(q => q.EmployeeBDUId == EmployeeBDUId && q.Active == true && q.Archived == false && q.DocumentType == "employeeAD").FirstOrDefault();
+            return response;
+        }
+    }
+}
